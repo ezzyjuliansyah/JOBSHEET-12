96a3f6958c61e39452e1e17c8827d428551748e3
ezzyjuliansyah <ezzyjuliansyah1@gmail.com>
2025-11-27 08:24:35 +0700

    first commit

--- Pertemuan14/Kafe09.java
+++ Pertemuan14/Kafe09.java
@@ -1,10 +1,8 @@
 package Pertemuan14;
 
 public class Kafe09 {
-    public static void main(String[] args) {
-        Menu();   
-    }
     public static void Menu() {
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam   - Rp 15.000");
         System.out.println("2. Cappucino    - Rp 20.000");
@@ -14,9 +12,8 @@ public class Kafe09 {
         System.out.println("6. Mie Goreng   - Rp 18.000");
         System.out.println("7. Nasi Goreng  - Rp 20.000");
         System.out.println("================================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        System.out.println("Silakan pilih menu yang Anda inginkan."); }
+    public static void main(String[] args) {
+        Menu();   
     }
-
 }
-
-
