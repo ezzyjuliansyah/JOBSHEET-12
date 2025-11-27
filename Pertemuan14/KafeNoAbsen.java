0bec548b03419a4b439595332e0aa271588175dd
ezzyjuliansyah <ezzyjuliansyah1@gmail.com>
2025-11-27 08:07:39 +0700

    first commit

---
+++ Pertemuan14/KafeNoAbsen.java
@@ -0,0 +1,22 @@
+package Pertemuan14;
+
+public class KafeNoAbsen {
+    public static void main(String[] args) {
+        Menu();   
+    }
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam   - Rp 15.000");
+        System.out.println("2. Cappucino    - Rp 20.000");
+        System.out.println("3. Latte        - Rp 22.000");
+        System.out.println("4. Teh Tarik    - Rp 10.000");
+        System.out.println("5. Roti Bakar   - Rp 12.000");
+        System.out.println("6. Mie Goreng   - Rp 18.000");
+        System.out.println("7. Nasi Goreng  - Rp 20.000");
+        System.out.println("================================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+}
+
+
